158a0e317451fc43f029a15873ed8ee95f3302e0
Juan Pablo Guevara <Juanpi@Juans-MacBook-Pro.local>
2019-05-09 16:04:27 -0500

    InflateLayout

--- InflateLayout/app/src/main/java/com/example/inflatelayout/MainActivity.java
+++ InflateLayout/app/src/main/java/com/example/inflatelayout/MainActivity.java
@@ -3,22 +3,35 @@ package com.example.inflatelayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    EditText editText;
+    String datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText =(EditText)findViewById(R.id.EditText);
+        editText.setText("asdasds");
     }
 
     public void onClickLeft(View view) {
         setContentView(R.layout.activity_main2);
+        datos = editText.getText().toString();
+        //editText.setText("");
+        textView= (TextView)findViewById(R.id.textView);
+        textView.setText("Dato: "+datos);
+
 
     }
 
     public void onClickRight(View view) {
         setContentView(R.layout.activity_main);
+        datos="";
 
     }
 
